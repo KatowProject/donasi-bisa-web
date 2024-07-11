4ff4b07f5de9edf6614ccd47ce981208daf3a5a7
KatowProject <naufalfaqih443@gmail.com>
2024-07-11 12:35:11 +0700

    chore: Add conditional rendering for fundraising cards
    This commit adds conditional rendering logic to the `+page.svelte` file to display a message when there are no fundraising cards available. If the `data.props.galangs` array is empty, the message "Belum Ada Penggalangan Dana" is shown. Otherwise, the cards are rendered using a loop through the `data` array. Each card includes an image, title, description, and a button to view more details.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (chore for non-functional changes, feat for new features) and provides a clear and concise description of the code changes.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,28 @@
 	</div>
 
 	<div class="col-12">
-		<div class="row"></div>
+		<div class="row">
+			<!-- if data length is 0, set "Belum Ada Penggalangan Dana" -->
+			{#if data.props.galangs.length === 0}
+				<div class="col-12">
+					<p>Belum Ada Penggalangan Dana</p>
+				</div>
+			{:else}
+				<!-- if data length is not 0, loop through data -->
+				{#each data as item}
+					<div class="col-md-4">
+						<div class="card">
+							<img src={item.image} class="card-img-top" alt={item.title} />
+							<div class="card-body">
+								<h5 class="card-title">{item.title}</h5>
+								<p class="card-text">{item.description}</p>
+								<a href="/penggalangan-dana/{item.id}" class="btn btn-primary">Lihat Detail</a>
+							</div>
+						</div>
+					</div>
+				{/each}
+			{/if}
+		</div>
 	</div>
 </div>
 
